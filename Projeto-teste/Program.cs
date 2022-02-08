1aa5bf7cc39f1ddd2db85f4e72ce81b98d270388
Alex Bom Fim <alexdejesusbomfim@outlook.com>
2022-02-08 02:09:35 -0300

    Desejando um bom dia

--- Projeto-teste/Program.cs
+++ Projeto-teste/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Bom dia!");
 Console.WriteLine("Como você está hoje?");
+Console.WriteLine("Tenha um bom dia!");
 
 Console.ReadLine();
